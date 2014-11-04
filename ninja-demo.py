5f87b6fb3997fcdf36baaaa654d09af296b70145
Eric Kern <ekern@mitre.org>
2014-11-04 16:24:38 -0500

    Refactored to use context managers

--- ninja-demo.py
+++ ninja-demo.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Usage:
-    ninja-demo.py <host> [--username username]
+    ninja-demo.py [--username username] <host> <script>
 """
 import contextlib
 import docopt
@@ -9,6 +9,15 @@ import paramiko
 import sys
 import threading
 import tty
+
+@contextlib.contextmanager
+def joining(thread):
+    thread.start()
+
+    try:
+        yield thread
+    finally:
+        thread.join()
 
 def display_thread(channel):
     while True:
@@ -23,6 +32,7 @@ def display_thread(channel):
 def main():
     options = docopt.docopt(__doc__)
     host = options['<host>']
+    script = options['<script>']
 
     if options['--username']:
         username = options['--username']
@@ -34,19 +44,14 @@ def main():
 
     client.connect(host, username = username)
     channel = client.invoke_shell()
-    t = threading.Thread(target = display_thread, args = [channel])
-    t.start()
+    
+    with joining(threading.Thread(target = display_thread, args = [channel])):
+        with contextlib.closing(channel):
+            fd = sys.stdin.fileno()
+            tty.setcbreak(fd) # echo off
 
-    fd = sys.stdin.fileno()
-    tty.setcbreak(fd) # echo off
-
-    try:
-        while True:
-            channel.send(sys.stdin.read(1))
-    except KeyboardInterrupt:
-        channel.close()
-        client.close()
-        t.join()
+            while True:
+                channel.send(sys.stdin.read(1))
 
 if __name__ == '__main__':
     main()
